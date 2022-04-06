e61ccd37efb5f2faf1afcc3d278e0d9163a572ee
Gabriel Mateus <gmsf19@gmail.com>
2022-04-06 12:02:43 -0300

    create page main of boilerplate and create test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,20 @@
-const Main = () => <div></div>;
+import * as S from './styles';
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS, Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor em frente a uma tela com códigos escritos."
+    />
+  </S.Wrapper>
+);
 
 export default Main;
