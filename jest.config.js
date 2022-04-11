719ca9327f0f9d72034b1027e66ee6736111754c
Gabriel Mateus <gmsf19@gmail.com>
2022-04-11 20:40:29 -0300

    adjustments in coverage tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.tsx(x)'],
+  collectCoverageFrom: ['src/**/*.tsx(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 };
